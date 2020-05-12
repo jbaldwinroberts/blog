d51fe955f0a4b25a657f5065ae9774b23fb6620a
josephroberts <j.baldwin.roberts@gmail.com>
2020-05-12 08:40:41 +0100

    finished

---
+++ client/src/CommentList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default ({ comments }) => {
+  const renderedComments = Object.values(comments).map((comment) => {
+    let content = comment.content;
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    } else if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
